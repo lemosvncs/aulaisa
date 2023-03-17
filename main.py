b85bef803813f958724ca7f5312d1182e4a5b342
Vini Lemos <lemos.vncs@gmail.com>
2023-03-17 15:59:19 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+# COMO FAZER O VENV
+# Abra o CMD ou Powershell
+# VSCode: Terminal > Novo terminal
+
+# Primeiro:
+# py -m venv .venv
+
+# Depois:
+# . .venv/Scripts/activate
+
+import pandas as pd
+
+df = pd.read_csv()
+
+# 1 - Identifique a média de gastos ("LG_VALOR_PREESTABELECIDO") para todo o banco.
+
+# 2 - Identifique a média de gastos com os homens e a média de gastos com as mulheres
+
+# a) com if
+
+# b) com a função .filter() do pandas
+
+# DESAFIO
+
+# Faça um gráfico da média de gastos usando o matplotlib
+
+# https://matplotlib.org/
